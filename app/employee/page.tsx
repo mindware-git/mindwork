2ce63434f078d8887d154ada2f2481cc228b16c7
Kwanghoon Son <kwangson@yahoo.com>
2023-05-15 22:01:29 +0900

    Add more pages

--- app/employee/page.tsx
+++ app/employee/page.tsx
@@ -1,7 +1,7 @@
-export default function Home() {
+export default function Employee() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      this is the main
+      Hello employee!
     </main>
   )
 }
--- app/employer/page.tsx
+++ app/employer/page.tsx
@@ -1,7 +1,7 @@
-export default function Home() {
+export default function Employer() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      this is the main
+      Hello employer!
     </main>
   )
 }
